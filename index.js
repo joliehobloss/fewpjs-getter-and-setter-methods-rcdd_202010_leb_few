24beadafa9cdf40d102995e7b0d7a7fbfbe1c33a
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-11 13:11:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ class Circle{
   }
   
   
-  set diameter(r){
-    this.radius=r/2
+  set diameter(d){
+    this.radius=d/2;
   }
   
-  set circumference(){
+  set circumference(c){
+    this.radius=c/(Math.PI*2);
    
   }
-   set area(){
+   /*set area(){
    
   }*/
   
